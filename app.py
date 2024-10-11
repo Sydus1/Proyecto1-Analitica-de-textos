d64e0400870b3fd675a5ad4954fdf137aa94eb35
Sydus1 <j.sierraa@uniandes.edu.co>
2024-10-11 17:17:24 -0500

    Fix logging app.py

--- app.py
+++ app.py
@@ -4,11 +4,25 @@ import numpy as np
 import logging
 
 # Configurar el logging
-logging.basicConfig(
-    filename='application.log',
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Handler para el archivo de logs
+file_handler = logging.FileHandler('app.log')
+file_handler.setLevel(logging.INFO)
+
+# Handler para la consola
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+# Formato para los logs
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# Agregar los handlers al logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
 
 app = Flask(__name__)
 
@@ -24,16 +38,16 @@ def index():
 @app.route('/predict_web', methods=['POST'])
 def predict_web():
     try:
-        logging.info("Solicitud recibida en /predict_web")
+        logger.info("Solicitud recibida en /predict_web")
         data = request.get_json(force=True)
         instances = data['instances']
         predictions = pipeline.predict(instances)
         probabilities = pipeline.predict_proba(instances).max(axis=1)
         results = [{'prediction': str(pred), 'probability': float(prob)} for pred, prob in zip(predictions, probabilities)]
-        logging.info(f"Predicciones realizadas: {results}")
+        logger.info(f"Predicciones realizadas: {results}")
         return jsonify(results)
     except Exception as e:
-        logging.error(f"Error en /predict_web: {e}")
+        logger.error(f"Error en /predict_web: {e}")
         return jsonify({'error': 'Ocurrió un error al procesar la predicción.'}), 500
 
 if __name__ == '__main__':
